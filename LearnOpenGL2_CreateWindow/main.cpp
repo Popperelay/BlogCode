d631e7bd9a5938d66319bed1349d21823ff4ff33
popperelay <825103209@qq.com>
2017-10-14 20:47:36 +0800

    first Test

---
+++ LearnOpenGL2_CreateWindow/main.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+//GLEW
+#define GLEW_STATIC
+#include<GL/glew.h>
+//GLFW
+#include<GLFW/glfw3.h>
+//窗口尺寸
+const GLuint WIDTH = 800, HEIGHT = 600;
+//函数声明
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+int main() {
+	//初始化GLFW
+	glfwInit();
+	//GLFW配置
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //主版本号
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //次版本号
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 核心开发模式
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE); //窗口尺寸不可变
+											  //创建能够使用GLFW函数的窗口对象（指针）
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	//创建窗口环境
+	glfwMakeContextCurrent(window);
+	//注册回调函数
+	glfwSetKeyCallback(window, key_callback);
+	//初始化GLEW
+	glewExperimental = GL_TRUE; //保证使用现代技术管理OpenGL机能
+	if (glewInit() != GLEW_OK) {
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+	//设置渲染窗口位置大小
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+	//渲染循环（游戏循环）
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents(); //检测是否有事件被触发（按键按下、鼠标移动）
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window); //交换屏幕缓存
+	}
+	//结束CLFW，释放由GLFW分配的资源
+	glfwTerminate();
+	return 0;
+}
+//按键回调函数
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+	if (key == GLFW_KEY_ESCAPE&&action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+}
