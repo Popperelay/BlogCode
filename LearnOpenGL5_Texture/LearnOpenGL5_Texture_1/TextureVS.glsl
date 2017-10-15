fbeec97a17fc7db1df63bf1d556d360ecc62b25a
Popperelay <825103209@qq.com>
2017-10-15 10:44:41 +0800

    Add files via upload
    LearnOpenGL5 Texture

---
+++ LearnOpenGL5_Texture/LearnOpenGL5_Texture_1/TextureVS.glsl
@@ -0,0 +1,12 @@
+//顶点着色器
+#version 330 core
+layout (location=0) in vec3 position;  //顶点位置变量的属性位置值为0
+layout (location=1) in vec3 color;     //顶点颜色变量的属性位置值为1
+layout (location=2) in vec2 texCoord;  //顶点纹理坐标的属性位置值为2
+out vec3 ourColor;
+out vec2 TexCoord;
+void main(){
+	gl_Position=vec4(position,1.0f);
+	ourColor=color;
+	TexCoord=texCoord;
+}
---
+++ LearnOpenGL5_Texture/LearnOpenGL5_Texture_2/main.cpp
@@ -0,0 +1,176 @@
+#include "shader.h"
+#include <iostream>
+//GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+//GLFW
+#include <GLFW/glfw3.h>
+//SOIL
+#include <SOIL/SOIL.h>
+//窗口尺寸
+const GLuint WIDTH = 800, HEIGHT = 600;
+//函数声明
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+int main() {
+	//初始化GLFW
+	glfwInit();
+	//GLFW配置
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //主版本号
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //次版本号
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 核心开发模式
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE); //窗口尺寸不可变
+											  //创建能够使用GLFW函数的窗口对象（指针）
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	//创建窗口环境
+	glfwMakeContextCurrent(window);
+	//注册回调函数
+	glfwSetKeyCallback(window, key_callback);
+	//初始化GLEW
+	glewExperimental = GL_TRUE; //保证使用现代技术管理OpenGL机能
+	if (glewInit() != GLEW_OK) {
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+	//设置渲染窗口位置大小
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+	//三角形顶点的坐标、颜色、纹理坐标
+	GLfloat vertices[] = {
+		//位置坐标         //颜色           //纹理坐标
+		0.5f, 0.5f, 0.0f,  1.0f,0.0f,0.0f,  1.0f,1.0f,
+		0.5f, -0.5f,0.0f,  0.0f,1.0f,0.0f,  1.0f,0.0f,
+		-0.5f,-0.5f,0.0f,  0.0f,0.0f,1.0f,  0.0f,0.0f,
+		-0.5f,0.5f, 0.0f,  1.0f,1.0f,0.0f,  0.0f,1.0f
+	};
+	//顶点索引
+	GLuint indices[] = {
+		0,1,3,  //第一个三角形
+		1,2,3   //第二个三角形
+	};
+	GLuint VAO, VBO, EBO;
+	//生成顶点数组对象VAO，记录数据的存储和如何使用的细节信息
+	glGenVertexArrays(1, &VAO);
+	//生顶点缓冲对象VBO，负责顶点数据的存储
+	glGenBuffers(1, &VBO);
+	//生成索引缓冲对象EBO，负责索引数据的存储
+	glGenBuffers(1, &EBO);
+	//给顶点数组对象绑定目标(顶点数组，不需要参数)
+	glBindVertexArray(VAO);
+	//给顶点缓冲对象绑定目标（数组内存）
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	//给索引缓冲对象绑定目标（索引数组内存）
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//把顶点数据复制到显卡的缓冲内存中
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//把索引数据复制到显卡的缓冲内存中
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	//告诉OpenGL如何解析显存中的顶点位置属性数据
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+	//打开顶点位置属性数组
+	glEnableVertexAttribArray(0);
+	//告诉OpenGL如何解析显存中的顶点颜色属性数据
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	//打开顶点颜色属性数组
+	glEnableVertexAttribArray(1);
+	//告诉OpenGL如何解析显存中的顶点纹理坐标属性数据
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	//打开顶点纹理坐标属性数组
+	glEnableVertexAttribArray(2);
+	//解绑VBO、EBO
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	//解绑VAO
+	glBindVertexArray(0);
+	GLuint texture1;
+	//生成纹理对象texture1
+	glGenTextures(1, &texture1);
+	//给纹理对象绑定目标（2D纹理）
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	int iamgeWidth, iamgeHeight;
+	//加载纹理图像
+	unsigned char* image = SOIL_load_image("container.jpg", &iamgeWidth, &iamgeHeight, 0, SOIL_LOAD_RGB);
+	//生成2D纹理
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, iamgeWidth, iamgeHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	//释放纹理数据内存
+	SOIL_free_image_data(image);
+	//设置纹理s和t方向的环绕方式
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	//设置纹理放大和缩写时的过滤方式（纹素过滤、mipmap边界过滤）
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	//生成多级渐远纹理
+	glGenerateMipmap(GL_TEXTURE_2D);
+	//解绑纹理对象
+	glBindTexture(GL_TEXTURE_2D, 0);
+	GLuint texture2;
+	//生成纹理对象texture2
+	glGenTextures(1, &texture2);
+	//给纹理对象绑定目标（2D纹理）
+	glBindTexture(GL_TEXTURE_2D, texture2);
+	//加载纹理图像
+	image = SOIL_load_image("awesomeface.png", &iamgeWidth, &iamgeHeight, 0, SOIL_LOAD_RGB);
+	//生成2D纹理
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, iamgeWidth, iamgeHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	//释放纹理数据内存
+	SOIL_free_image_data(image);
+	//设置纹理s和t方向的环绕方式
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_LINEAR);
+	//设置纹理放大和缩写时的过滤方式（纹素过滤、mipmap边界过滤）
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	//生成多级渐远纹理
+	glGenerateMipmap(GL_TEXTURE_2D);
+	//解绑纹理对象
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	//定义自定义着色器类shader的对象
+	shader myshader("TextureVS.glsl", "TextureFS.glsl");
+	//渲染循环（游戏循环）
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents(); //检测是否有事件被触发（按键按下、鼠标移动）
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		//激活着色器程序对象
+		myshader.Use();
+
+		//激活纹理单元
+		glActiveTexture(GL_TEXTURE0);
+		//获取uniform变量位置
+		GLint uniformLocation1 = glGetUniformLocation(myshader.shaderProgram, "ourTexture1");
+		//设置采样器的值（对应纹理单元值）
+		glUniform1i(uniformLocation1, 0);
+		//绑定纹理对象
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		GLint uniformLocation2 = glGetUniformLocation(myshader.shaderProgram, "ourTexture2");
+		glUniform1i(uniformLocation2, 1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+		//绑定VAO，完成顶点输入初始化
+		glBindVertexArray(VAO);
+		//绘图
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		//解绑VAO
+		glBindVertexArray(0);
+		glfwSwapBuffers(window); //交换颜色缓存
+	}
+	//删除VAO、VBO、EBO
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+	//结束CLFW，释放由GLFW分配的资源
+	glfwTerminate();
+	return 0;
+}
+//按键回调函数
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+	if (key == GLFW_KEY_ESCAPE&&action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+}
